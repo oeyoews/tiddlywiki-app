efcc7afa6d9d7ab6a6d259a69aa0fa24778f21e5
oeyoews <jyao4783@gmail.com>
2025-03-24 18:16:49 +0800

    refactor: migrate wiki utility functions to TypeScript

--- src/utils/wiki.ts
+++ src/utils/wiki.ts
@@ -1,12 +1,12 @@
-export const wikiInitArgs = (path) => [path, '--init', 'server'];
-export const wikiStartupArgs = (path, port) => [
+export const wikiInitArgs = (path: string) => [path, '--init', 'server'];
+export const wikiStartupArgs = (path: string, port: number) => [
   path,
   '--listen',
   `port=${port}`,
   'root-tiddler=$:/core/save/all-external-js',
 ];
 
-export const wikiBuildArgs = (path) => [path, '--build', 'index'];
+export const wikiBuildArgs = (path: string) => [path, '--build', 'index'];
 export const buildIndexHTMLArgs = [
   '--render',
   '$:/plugins/tiddlywiki/tiddlyweb/save/offline',
