0680ac5a15662f1eab304262a8dfaf2943f9c635
oeyoews <jyao4783@gmail.com>
2025-03-28 21:56:28 +0800

    perf: add cache for template download

--- src/utils/downloadTpl.ts
+++ src/utils/downloadTpl.ts
@@ -4,8 +4,8 @@ import fs from 'fs';
 const tempDir = app.getPath('temp'); // 获取系统的临时目录
 
 import { log } from '@/utils/logger';
+const cacheDuration = 24 * 60 * 60 * 1000; // 24小时
 
-// 缓存
 export const downloadTpl = (
   tpl: [content: string, label: string],
   cbl: Function
@@ -13,11 +13,29 @@ export const downloadTpl = (
   const content = tpl[1];
   const label = tpl[0];
   const filePath = path.join(tempDir, `${label}.html`);
+
+  // 检查文件是否存在且是否过期
   try {
+    if (fs.existsSync(filePath)) {
+      const stats = fs.statSync(filePath);
+      const lastModified = stats.mtime.getTime();
+      const currentTime = Date.now();
+
+      // 如果文件在24小时内修改过，则直接复用缓存
+      if (currentTime - lastModified < cacheDuration) {
+        log.log(`${filePath} template is still valid, using cached version.`);
+        cbl(filePath);
+        return;
+      } else {
+        log.log(`${filePath} template is outdated, downloading again.`);
+      }
+    }
+
+    // 如果文件不存在或过期，下载新的模板
     if (content.startsWith('http')) {
       const request = net.request(content);
+      log.log(`${content} template is downloading!`);
 
-      log.log(`${content} template is downloading !`);
       request.on('response', (response) => {
         let data = '';
 
@@ -28,7 +46,7 @@ export const downloadTpl = (
         response.on('end', () => {
           fs.writeFileSync(filePath, data, 'utf-8');
           cbl(filePath);
-          log.log(`${filePath} template has donwloaded !`);
+          log.log(`${filePath} template has downloaded!`);
         });
       });
 
