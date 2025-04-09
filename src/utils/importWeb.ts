211a3285a7a720866dbf653751662ea16d410587
oeyoews <jyao4783@gmail.com>
2025-04-09 16:59:37 +0800

    perf: update importweb

--- src/utils/importWeb.ts
+++ src/utils/importWeb.ts
@@ -2,7 +2,7 @@ import { TWProtocol } from '@/main';
 import { BrowserWindow, dialog } from 'electron';
 import { log } from './logger';
 
-// start or second-instance or ready
+// second-instance or ready
 // tiddlywiki://?_source=web&title=999
 export function importWeb(win: BrowserWindow, argv: string[], _url?: string) {
   if (!win) {
@@ -20,6 +20,11 @@ export function importWeb(win: BrowserWindow, argv: string[], _url?: string) {
   if (url) {
     const info = new URL(url);
     const source = info.searchParams.get('_source');
+    if (source !== 'web') {
+      log.info('not from web');
+      return;
+    }
+    // test: 考虑为空的情况
     const tags = info.searchParams.getAll('tags');
     const {
       _source,
@@ -28,13 +33,11 @@ export function importWeb(win: BrowserWindow, argv: string[], _url?: string) {
     } = Object.fromEntries(info.searchParams.entries());
 
     // 校验来源
-    if (source === 'web') {
-      log.info('Begin import tiddler from', source);
-      // 注意， 这里需要等待render.js 执行完毕
-      setTimeout(() => {
-        win.webContents.send('open-url', { ...tiddler, tags });
-      }, 100);
-    }
+    log.info('Begin import tiddler from', source);
+    // 注意， 这里需要等待render.js 执行完毕
+    setTimeout(() => {
+      win.webContents.send('open-url', { ...tiddler, tags });
+    }, 100);
   } else {
     log.info('no get valid url', url, _url);
   }
