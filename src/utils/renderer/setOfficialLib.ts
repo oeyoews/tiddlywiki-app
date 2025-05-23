ed50f396f33993c8dabd2eb500f5c7186f4afe21
oeyoews <ydj@quaitra.com>
2025-05-23 09:49:55 +0800

    fix: update official lib

--- src/utils/renderer/setOfficialLib.ts
+++ src/utils/renderer/setOfficialLib.ts
@@ -3,11 +3,17 @@ export function setOfficialLib() {
   // enable official plugin library
   const pluginLibraryUrl = `https://tiddlywiki.com/library/v${$tw.version}/index.html`;
   const officialLibraryTiddler = '$:/config/OfficialPluginLibrary';
-  if ($tw.wiki.getTiddler('url')?.fields?.enable === 'no') {
-    $tw.wiki.setText(officialLibraryTiddler, 'url', null, pluginLibraryUrl, {
-      suppressTimestamp: true,
-    });
-    $tw.wiki.setText(officialLibraryTiddler, 'enable', null, 'yes', {
+  if ($tw.wiki.getTiddler(officialLibraryTiddler)?.fields?.enabled === 'no') {
+    $tw.wiki.setText(
+      officialLibraryTiddler,
+      officialLibraryTiddler,
+      null,
+      pluginLibraryUrl,
+      {
+        suppressTimestamp: true,
+      }
+    );
+    $tw.wiki.setText(officialLibraryTiddler, 'enabled', null, 'yes', {
       suppressTimestamp: true,
     });
   }
